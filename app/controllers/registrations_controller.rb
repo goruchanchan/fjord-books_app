50b3e64f575303e547b17f5cd712450685ec0a5e
goruchan <ppcfr481@yahoo.co.jp>
2022-08-18 08:38:46 +0900

    update ローカライズファイル構成変更
    ja.ymlは、共通項目っぽいので、userモデルのものをtranslationに移動

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   protected
-
+    # https://github.com/heartcombo/devise/wiki/How-To:-Customize-the-redirect-after-a-user-edits-their-profile
     def after_update_path_for(resource)
       user_path(resource)
     end
