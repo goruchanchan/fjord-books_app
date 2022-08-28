d38686c1d4e2932a80406649eb7c927cbf4c24ed
goruchan <ppcfr481@yahoo.co.jp>
2022-08-28 23:02:01 +0900

    add フォロー用テーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_135031) do
+ActiveRecord::Schema.define(version: 2022_08_28_132659) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2022_08_22_135031) do
     t.string "picture"
   end
 
+  create_table "follows", force: :cascade do |t|
+    t.integer "userID"
+    t.integer "followed_userID"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_userID"], name: "index_follows_on_followed_userID"
+    t.index ["userID"], name: "index_follows_on_userID"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +76,6 @@ ActiveRecord::Schema.define(version: 2022_08_22_135031) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "follows", "users", column: "followed_userID"
+  add_foreign_key "follows", "users", column: "userID"
 end
