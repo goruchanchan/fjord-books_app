2a70c46efb74c06e305fb3eb48934b11b56e39fe
goruchan <ppcfr481@yahoo.co.jp>
2022-08-31 23:16:12 +0900

    clean 不要コード削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit]
+  before_action :set_user, only: %i[show]
 
   def index
     @users = User.order(:id).page(params[:page])
   end
 
   def show; end
-
-  def edit; end
 
   private
 
