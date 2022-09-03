db423391c423bb0fc6979a2277104aec323009b9
goruchan <ppcfr481@yahoo.co.jp>
2022-09-03 22:56:10 +0900

    add フォロー一覧、フォロワー一覧の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit]
+  before_action :set_user, only: %i[show edit following followers]
 
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
   def show; end
 
   def edit; end
+
+  def following
+    @title = User.human_attribute_name(:follow)
+    @users  = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = User.human_attribute_name(:follower)
+    @users = @user.followers
+    render 'show_follow'
+  end
 
   private
 
