0fe3cd20ca584bb7d6f978d1088b1a9724c3b393
goruchan <ppcfr481@yahoo.co.jp>
2022-09-13 23:17:39 +0900

    add ブックのコメント編集機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@
 
 class CommentsController < ApplicationController
   before_action :set_parameter, only: %i[create destroy]
-  before_action :set_comment, only: %i[destroy]
+  before_action :set_redirect_path, only: %i[create destroy update]
+  before_action :set_comment, only: %i[destroy edit update]
+  before_action :set_commentable, only: %i[edit]
+
+  def edit; end
 
   # POST /comments
   # POST /comments.json
@@ -11,9 +15,15 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @redirect_path, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
         format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(content: params[:content])
+        format.html { redirect_to @redirect_path, notice: t('controllers.common.notice_update', name: Comment.model_name.human) }
+        format.json { render :show, status: :ok, location: @comment }
       end
     end
   end
@@ -32,12 +42,28 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def set_commentable
+    if request.path.match(/\/reports(\/)?/)
+      @commentable = Report.find(params[:report_id])
+    else
+      @commentable = Book.find(params[:book_id])
+    end
+  end
+
   def set_parameter
     if request.path.match(/\/reports(\/)?/)
       @comment = Report.find(params[:report_id]).comments.new(content: params[:content], user_id: current_user.id)
       @redirect_path = report_path(id: params[:report_id])
     else
       @comment = Book.find(params[:book_id]).comments.new(content: params[:content], user_id: current_user.id)
+      @redirect_path = book_path(id: params[:book_id])
+    end
+  end
+
+  def set_redirect_path
+    if request.path.match(/\/reports(\/)?/)
+      @redirect_path = report_path(id: params[:report_id])
+    else
       @redirect_path = book_path(id: params[:book_id])
     end
   end
