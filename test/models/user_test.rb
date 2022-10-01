2a4b64e96e748f8d8b5be5e3450755f386b92eb6
goruchan <ppcfr481@yahoo.co.jp>
2022-10-01 21:54:37 +0900

    update setupメソッド導入
    followメソッドとunfollowメソッドでテストコードの重複が出たのでDRYにした。

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,30 +5,28 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   fixtures :users
 
-  test '#name_or_email' do
-    user = users(:hoge)
-    assert_equal 'hoge@example.com', user.name_or_email
+  setup do
+    @user = users(:hoge)
+    @he = users(:he)
+    @her = users(:her)
+  end
 
-    user.name = 'hoge'
-    assert_equal 'hoge', user.name_or_email
+  test '#name_or_email' do
+    assert_equal 'hoge@example.com', @user.name_or_email
+    @user.name = 'hoge'
+    assert_equal 'hoge', @user.name_or_email
   end
 
   test '#follow' do
-    he = users(:he)
-    her = users(:her)
-
-    assert_not he.following?(her)
-    he.follow(her)
-    assert he.following?(her)
+    assert_not @he.following?(@her)
+    @he.follow(@her)
+    assert @he.following?(@her)
   end
 
   test '#unfollow' do
-    he = users(:he)
-    her = users(:her)
-
-    he.follow(her)
-    assert he.following?(her)
-    he.unfollow(her)
-    assert_not he.following?(her)
+    @he.follow(@her)
+    assert @he.following?(@her)
+    @he.unfollow(@her)
+    assert_not @he.following?(@her)
   end
 end
