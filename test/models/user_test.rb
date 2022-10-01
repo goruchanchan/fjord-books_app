9b5a5604e29ee5d18ede5c52167b0e398744bd89
goruchan <ppcfr481@yahoo.co.jp>
2022-10-01 21:23:18 +0900

    add followメソッドのテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "#name_or_email" do
+  test '#name_or_email' do
     user = User.new(email: 'hoge@example.com', name: '')
     assert_equal 'hoge@example.com', user.name_or_email
 
     user.name = 'hoge'
     assert_equal 'hoge', user.name_or_email
+  end
+
+  test '#follow' do
+    he = User.create!(email: 'he@example.com', password: 'password')
+    her = User.create!(email: 'her@example.com', password: 'password')
+
+    assert_not he.following?(her)
+    he.follow(her)
+    assert he.following?(her)
   end
 end
