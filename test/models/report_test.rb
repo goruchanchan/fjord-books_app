2a5d125fe82c479eabe0c445b052801ac23df071
goruchan <ppcfr481@yahoo.co.jp>
2022-10-02 21:55:35 +0900

    update reportモデルのeditableメソッドにfixtureを導入

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,14 +3,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  fixtures :reports
+  fixtures :users
+
+  setup do
+    @report = reports(:one)
+  end
+
   test '#editable' do
-    user = User.new(email: 'user@example.com', name: 'user')
-    user.reports.new(id:1, title: 'test')
-    report = user.reports.first
-
-    assert report.editable?(user)
-    other = User.new(email: 'other@example.com', name: 'other')
-    assert_not report.editable?(other)
-
+    assert @report.editable?(users(:hoge))
+    assert_not @report.editable?(users(:he))
   end
 end
