3c8689bf726e24e1e49d087a7d39d9584aefd875
goruchan <ppcfr481@yahoo.co.jp>
2022-10-02 21:38:49 +0900

    add reportモデルのeditableメソッドのテスト追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable' do
+    user = User.new(email: 'user@example.com', name: 'user')
+    user.reports.new(id:1, title: 'test')
+    report = user.reports.first
+
+    assert report.editable?(user)
+    other = User.new(email: 'other@example.com', name: 'other')
+    assert_not report.editable?(other)
+
+  end
 end
