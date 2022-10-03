02e1df0ba483c94fbd2b6848db0616338e09fc57
goruchan <ppcfr481@yahoo.co.jp>
2022-10-03 22:19:47 +0900

    add Reportモデルの日報更新のシステムテストを追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -2,6 +2,8 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
+    @report = reports(:rails_test)
+
     visit root_url
     fill_in 'Eメール', with: 'hoge@example.com'
     fill_in 'パスワード', with: 'password'
@@ -24,6 +26,20 @@ class ReportsTest < ApplicationSystemTestCase
     assert_text '日報が作成されました。'
     assert_text 'Railsでテストを書く'
     assert_text 'createをやってみた'
+    click_on '戻る'
+  end
+
+  test 'updating a Report' do
+    visit reports_url
+    click_on '編集'
+
+    fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: @report.content
+    click_on '更新する'
+
+    assert_text '日報が更新されました。'
+    assert_text 'Railsでテストを書く'
+    assert_text 'updateやってみた'
     click_on '戻る'
   end
 
