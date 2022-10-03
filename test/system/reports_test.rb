e7e7a027bd5d2bdd8f98380aa66e75be8bb34d5b
goruchan <ppcfr481@yahoo.co.jp>
2022-10-03 22:07:24 +0900

    add 日報モデルの日報新規作成のシステムテスト追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -8,9 +8,23 @@ class ReportsTest < ApplicationSystemTestCase
     click_on 'ログイン'
   end
 
-  test 'visiting the index' do
+  test 'visiting the Report index' do
     visit reports_url
     assert_selector 'h1', text: '日報'
+  end
+
+  test 'creating a Report' do
+    visit reports_url
+    click_on '新規作成'
+
+    fill_in 'タイトル', with: 'Railsでテストを書く'
+    fill_in '内容', with: 'createをやってみた'
+    click_on '登録する'
+
+    assert_text '日報が作成されました。'
+    assert_text 'Railsでテストを書く'
+    assert_text 'createをやってみた'
+    click_on '戻る'
   end
 
 end
